c4d9c9b00393e332193714103ee8305a722b4ca8
thiagomachadoo <thiagodasilvamachadoo44@gmail.com>
2021-09-21 11:40:21 -0300

    adicionando eventos e interaçoes juntamente as novas funcoes implementadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,62 @@ Case sensitive = Reconhece letras maiusculas e minusculas
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false 
+let emailOk = false 
+let assuntoOk = false 
+let mapa = document.querySelector('#mapa')
 
 nome.style.width = '100%'
 email.style.width = '100%'
 
 function validaNome(){
-    if(nome.Value.length < 3){
-        alert('nome inválido')
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.length < 3){
+       txtNome.innerHTML = 'Nome Inválido'
+       txtNome.style.color = 'red'
     }
+    else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    }else{
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if(assunto.value.length >= 100){
+        txtAssunto.innerHTML = 'Texto muito extenso, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    }else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+function enviar(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Formulário enviado com sucesso!!')
+    }else{
+        alert('Preencha as informações corretamente antes de enviar')
+    }
+}
+function mapaZoom(){
+mapa.style.width = '800px'
+mapa.style.heigth = '600px'
+}
+function mapaNormal(){
+    mapa.style.width = '450px'
+    mapa.style.heigth = '250px'
 }
